140b64bf2fdae95cef29d04315c13af55cfa4b20
Donn Felker <donn@donnfelker.com>
2014-03-22 13:16:39 -0700

    Adding show action

--- routes/events.js
+++ routes/events.js
@@ -1,11 +1,13 @@
-var Boom    = require('boom');
-var Event   = require('../models/event').Event;
+var Boom    = require('boom');                  // HTTP Errors
+var Joi     = require('joi');                   // Validation
+var Event   = require('../models/event').Event; // Mongoose ODM
 
 module.exports = exports = function(server) {
 
   console.log('Loading events routes');
   exports.index(server);
   exports.new(server);
+  exports.show(server);
 }
 
 exports.index = function(server) {
@@ -25,6 +27,12 @@ exports.index = function(server) {
     });
 };
 
+/**
+ * POST /new
+ * Creates a new event in the datastore.
+ *
+ * @param server - The Hapi Serve
+ */
 exports.new = function(server) {
   // POST /events
   var event;
@@ -42,14 +50,43 @@ exports.new = function(server) {
 
       event.save(function(err) {
         if(!err) {
-          reply(event).created('/events/' + event._id);
+          reply(event).created('/events/' + event._id);    // HTTP 201
         } else {
           ;
-          reply(Boom.forbidden(getErrorMessageFrom(err)));
+          reply(Boom.forbidden(getErrorMessageFrom(err))); // HTTP 403
         }
       });
     }
   });
+}
+
+exports.show = function(server) {
+
+    server.route({
+        method:     'GET',
+        path:       '/events/{id}',
+        config: {
+            validate: {
+                path: {
+                    id: Joi.string().alphanum().min(5).required()
+                }
+            }
+        },
+        handler:    function (request, reply) {
+            Event.findById(request.params.id, function(err, event) {
+                if(!err && event) {
+                    reply(event);
+                } else if(err) {
+                    // Log it, but dont show the user, don't want to expose ourselves (think security)
+                    console.log(err);
+                    reply(Boom.notFound());
+                } else {
+
+                    reply(Boom.notFound());
+                }
+            });
+        }
+    })
 }
 
 function getErrorMessageFrom(err) {
