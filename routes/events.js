a321b12033bee3f929e7448ea84710c242ab9019
Donn Felker <donn@donnfelker.com>
2014-03-22 14:30:07 -0700

    Adding delete route

--- routes/events.js
+++ routes/events.js
@@ -1,29 +1,30 @@
-var Boom    = require('boom');                  // HTTP Errors
-var Joi     = require('joi');                   // Validation
-var Event   = require('../models/event').Event; // Mongoose ODM
+var Boom = require('boom');                  // HTTP Errors
+var Joi = require('joi');                   // Validation
+var Event = require('../models/event').Event; // Mongoose ODM
 
-module.exports = exports = function(server) {
+module.exports = exports = function (server) {
 
-  console.log('Loading events routes');
-  exports.index(server);
-  exports.new(server);
-  exports.show(server);
+    console.log('Loading events routes');
+    exports.index(server);
+    exports.new(server);
+    exports.show(server);
+    exports.delete(server);
 }
 
-exports.index = function(server) {
+exports.index = function (server) {
     // GET /events
     server.route({
-      method:   'GET',
-      path:     '/events',
-      handler:  function(request, reply) {
-        Event.find({}, function(err, events) {
-          if(!err) {
-            reply(events);
-          } else {
-            reply(Boom.badImplementation(err)); // 500 error
-          }
-        });
-      }
+        method: 'GET',
+        path: '/events',
+        handler: function (request, reply) {
+            Event.find({}, function (err, events) {
+                if (!err) {
+                    reply(events);
+                } else {
+                    reply(Boom.badImplementation(err)); // 500 error
+                }
+            });
+        }
     });
 };
 
@@ -33,38 +34,38 @@ exports.index = function(server) {
  *
  * @param server - The Hapi Serve
  */
-exports.new = function(server) {
-  // POST /events
-  var event;
-
-  server.route({
-    method:    'POST',
-    path:      '/events',
-    handler:   function(request, reply) {
-
-      event = new Event();
-      event.category = request.payload.category;
-      event.action   = request.payload.action;
-      event.label    = request.payload.label;
-      event.source   = request.info.remoteAddress;
-
-      event.save(function(err) {
-        if(!err) {
-          reply(event).created('/events/' + event._id);    // HTTP 201
-        } else {
-          ;
-          reply(Boom.forbidden(getErrorMessageFrom(err))); // HTTP 403
-        }
-      });
-    }
-  });
-}
-
-exports.show = function(server) {
+exports.new = function (server) {
+    // POST /events
+    var event;
 
     server.route({
-        method:     'GET',
-        path:       '/events/{id}',
+        method: 'POST',
+        path: '/events',
+        handler: function (request, reply) {
+
+            event = new Event();
+            event.category = request.payload.category;
+            event.action = request.payload.action;
+            event.label = request.payload.label;
+            event.source = request.info.remoteAddress;
+
+            event.save(function (err) {
+                if (!err) {
+                    reply(event).created('/events/' + event._id);    // HTTP 201
+                } else {
+                    ;
+                    reply(Boom.forbidden(getErrorMessageFrom(err))); // HTTP 403
+                }
+            });
+        }
+    });
+}
+
+exports.show = function (server) {
+
+    server.route({
+        method: 'GET',
+        path: '/events/{id}',
         config: {
             validate: {
                 path: {
@@ -72,11 +73,11 @@ exports.show = function(server) {
                 }
             }
         },
-        handler:    function (request, reply) {
-            Event.findById(request.params.id, function(err, event) {
-                if(!err && event) {
+        handler: function (request, reply) {
+            Event.findById(request.params.id, function (err, event) {
+                if (!err && event) {
                     reply(event);
-                } else if(err) {
+                } else if (err) {
                     // Log it, but dont show the user, don't want to expose ourselves (think security)
                     console.log(err);
                     reply(Boom.notFound());
@@ -89,18 +90,46 @@ exports.show = function(server) {
     })
 }
 
-function getErrorMessageFrom(err) {
-  var errorMessage = '';
-  var i;
+exports.delete = function (server) {
+    server.route({
+        method: 'DELETE',
+        path: '/events/{id}',
+        config: {
+            validate: {
+                path: {
+                    id: Joi.string().alphanum().min(5).required()
+                }
+            }
+        },
+        handler: function (request, reply) {
+            Event.findById(request.params.id, function(err, event) {
+                if(!err && event) {
+                    event.remove();
+                    reply({ message: "Event deleted successfully"});
+                } else if(!err) {
+                    // Couldn't find the object.
+                    reply(Boom.notFound());
+                } else {
+                    console.log(err);
+                    reply(Boom.badRequest("Could not delete Event"));
+                }
+            });
+        }
+    })
+};
 
-  if(err.errors) {
-    for(var prop in err.errors) {
-      errorMessage += err.errors[prop].message + ' '
+function getErrorMessageFrom(err) {
+    var errorMessage = '';
+    var i;
+
+    if (err.errors) {
+        for (var prop in err.errors) {
+            errorMessage += err.errors[prop].message + ' '
+        }
+
+    } else {
+        errorMessage = err.message;
     }
 
-  } else {
-    errorMessage = err.message;
-  }
-
-  return errorMessage;
+    return errorMessage;
 }
